eb6706fd2c5be058bfe9c1d8a4ebc53688271367
chayilDeborah <aniyikayeolamide3@gmail.com>
2023-06-11 03:19:46 +0100

    style: stylings and svgs

--- app/page.js
+++ app/page.js
@@ -1,176 +1,46 @@
 "use client";
-import React, { useState } from "react";
-import Select from "react-select";
-import Nav from "./component/Nav";
+
 import Image from "next/image";
-import menu from "./asset/menu.svg";
-import convert from "./asset/convert.svg";
-import equiv from "./asset/equiv.svg";
-import notice from "./asset/notice.svg";
-import tick from "./asset/tick.svg";
+import Nav from "./component/Nav";
+import groupmic from "./asset/groupmic.svg";
+import cropmusic from "./asset/cropmusic.png";
+import box from "./asset/box.svg";
+import star from "./asset/star.svg";
+import Footer from "./component/Footer";
+import mob from './asset/mob.svg';
+import mobmus from './asset/mobmus.png'
 
 export default function Home() {
-
-  const customStyles = {
-    control: (base, state) => ({
-      ...base,
-      background: "none",
-      borderRadius: "17px",
-      // Overwrittes the different states of border
-      borderColor: state.isFocused ? "#ECECEC" : "#ececec",
-      // Removes weird border around container
-      boxShadow: state.isFocused ? null : null,
-      "&:hover": {
-        // Overwrittes the different states of border
-        borderColor: state.isFocused ? "#26489E" : "#26489E",
-        
-      }
-    }),
-    indicatorSeparator: (base) => ({
-      ...base,
-      display: "none"
-    }),
-    placeholder: (base) => ({
-      ...base,
-      placeholder: "Select Token"
-    }),
-    indicatorsContainer: (base) => ({
-      ...base,
-      color: "#D5D5D6"
-    }),
-    menu: (base) => ({
-      ...base,
-      // background: "#2d2c2c94"
-      color: "#26489E"
-    }),
-    singleValue: (base) => ({
-      ...base,
-      color: "#26489E",
-      fontWeight: 700,
-      fontSize: "14px",
-      lineHeight: "17px"
-    })
-  };
-
-  const data = [
-    {
-      value: 1,
-      token: "ETH",
-      icon: "https://res.cloudinary.com/dmye53wps/image/upload/v1686398570/ETH_ndf4du.svg",
-    },
-    {
-      value: 2,
-      token: "AST",
-      icon: "https://res.cloudinary.com/dmye53wps/image/upload/v1686394877/AST_qd9pml.svg",
-    },
-  ];
-
-  const [selectedOption, setSelectedOption] = useState(null);
-  const [selectedOption2, setSelectedOption2] = useState(null);
-
-  // handle onChange event of the dropdown
-  const handleChange = (e) => {
-    setSelectedOption(e);
-  };
-  const handledChange = (e) => {
-    setSelectedOption2(e);
-  };
   return (
     <>
-      <Nav />
-      <div className="main">
-        <div className="container">
-          <div>
-            <div className="tab-flex">
-              <div className="swap-text">Swap</div>
-              <Image src={menu} alt="menu-icon" className="menu-icon" />
+      <div className="wrapper">
+        <Nav />
+        <div>
+          <div className="page-flex">
+            <Image src={groupmic} alt="svg" className="groupmic" />
+            <Image src={mob} alt="svg" className="mobsvg" />
+            <div className="content-group">
+              Content Creation & Aggregation Dex Built on Optimism
             </div>
-            <div className="first-token">
-              <div>
-                <input className="input"/>
-                <div className="token_equivalent">$220.07</div>
-              </div>
-    
-              <Select
-              id='select-id'
-                styles={customStyles}
-                value={selectedOption}
-                options={data}
-                onChange={handleChange}
-                getOptionLabel={(e) => (
-                  <div style={{ display: "flex", alignItems: "center" }} id="selectdiv-id">
-                    <Image src={e.icon} width={20} height={20} />
-                    <span style={{ marginLeft: 5 }}>{e.token}</span>
-                  </div>
-                )}
-              />
-            </div>
-            <div className="break-flex">
-              <div className="first-line"></div>
-              <Image src={convert} alt="convert" className="convert" />
-              <div className="sec-line"></div>
-            </div>
-            <div className="first-token">
-              <div>
-              <input className="input"/>
-                <div className="token_equivalent">$238.07</div>
-              </div>
-              <Select
-                id="select-id"
-                styles={customStyles}
-                value={selectedOption2}
-                options={data}
-                onChange={handledChange}
-                getOptionLabel={(e) => (
-                  <div
-                    style={{ display: "flex", alignItems: "center" }}
-                    id="selectdiv-id"
-                  >
-                    <Image src={e.icon} width={20} height={20} />
-                    <span style={{ marginLeft: 5 }}>{e.token}</span>
-                  </div>
-                )}
-              />
-            </div>
-            <div className="swap-btn-flex">
-            <button className="swap-connect-btn">Swap</button>
-            </div>
-            <div className="estimate-group">
-              <div className="estim">
-                <div className="estimate-flex">
-                  <div className="estim-text">Estimated Cost</div>
-                  <div className="estim-no">
-                    <Image src={equiv} alt="icon" className="equiv" />
-                    <div className="estim-num">$5.08</div>
-                  </div>
-                </div>
-                <div className="estimate-flex" id="estim-id">
-                  <div className="estim-text">Price Impact</div>
-                  <div className="estim-no">
-                    <div className="estim-num">0.00%</div>
-                  </div>
-                </div>
-                <div className="estimate-flex" id="estim-id">
-                  <div className="estim-text-flex">
-                    <div className="save">You Save</div>
-                    <Image src={notice} alt="note" className="note" />
-                  </div>
-                  <div className="estim-no">
-                    <Image src={tick} alt="icon" className="tick" />
-                    <div className="estim-num">$5.08</div>
-                  </div>
-                </div>
-                <div className="estimate-flex1" id="estim-id">
-                  <div className="estim-text">Minimum Received</div>
-                  <div className="estim-no">
-                    <div className="estim-num">22,000 AST</div>
-                  </div>
-                </div>
-              </div>
-            </div>
+            <Image src={cropmusic} alt="svg" className="grpmusic" />
+            <Image src={mobmus} alt="svg" className="mobmus" />
+          </div>
+          <div className="util">
+            Utilizing the Bribe Economy, Cheap Gas Fees powered by Optimism
+          </div>
+          <div className="btns-flex">
+            <Image src={box} alt="svg" className="box" />
+            <button className="launch-btn1" id="launch-id">
+              Launch App
+            </button>
+            <button className="white-btn">Whitepaper</button>
+          </div>
+          <div className="star-grp">
+            <Image src={star} alt="svg" className="star" />
           </div>
         </div>
       </div>
+      <Footer />
     </>
   );
 }

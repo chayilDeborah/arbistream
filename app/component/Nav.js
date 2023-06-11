86379b5b35088250bc1487d1df7eeb8bf6c2616a
chayilDeborah <aniyikayeolamide3@gmail.com>
2023-06-11 06:11:15 +0100

    feat: hamburger menu for responsiveness

--- app/component/Nav.js
+++ app/component/Nav.js
@@ -1,14 +1,14 @@
 import Image from "next/image"
 import logowhite from '../asset/logowhite.svg'
 import uparrow from '../asset/uparrow.svg';
-
+import hamburger from '../asset/hamburger.svg'
 
 const Nav = () => {
   return (
    <>
    <div className="nav-group">
     <Image src={logowhite} alt="Astrava" className="logowhite" />
-    
+    <Image src={hamburger} alt="menu" className="hamburger" />
     <div className="navbar-links">
       <div className="nav-home">Home</div>
       <div className="product-flex">

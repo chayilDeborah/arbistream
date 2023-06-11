f5b3dc40468c9d6cd86c29e5f8a94038a867fe11
chayilDeborah <aniyikayeolamide3@gmail.com>
2023-06-11 21:28:32 +0100

    feat: add nav links to devNav

--- app/component/DexNav.js
+++ app/component/DexNav.js
@@ -1,11 +1,28 @@
 import Image from "next/image"
 import navicon from '../asset/navicon.png'
+import uparrow from "../asset/uparrow.svg";
 
 const Nav = () => {
   return (
     <>
     <div className="nav-wrap">
         <Image src={navicon} alt="Arbistream-logo" className="logo"/>
+        <div className="navbar-links">
+            <div className="nav-home">Home</div>
+            <div className="product-flex">
+              <div className="nav-home" id="product">
+                Product
+              </div>
+              <Image src={uparrow} alt="icon" className="uparrow" />
+            </div>
+            <div className="product-flex">
+              <div className="nav-home" id="product">
+                Docs
+              </div>
+              <Image src={uparrow} alt="icon" className="uparrow" />
+            </div>
+            <div className="nav-home">Community</div>
+          </div>
         <button className="connect-btn">Connect Wallet</button>
     </div>
     </>

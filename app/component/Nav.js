e4fbba799c826e8f279e3a23f65fb1b4aaf8efe1
chayilDeborah <aniyikayeolamide3@gmail.com>
2023-06-12 11:03:07 +0100

    feat: nav styling

--- app/component/Nav.js
+++ app/component/Nav.js
@@ -1,30 +1,32 @@
 import Image from "next/image"
-import logowhite from '../asset/logowhite.svg'
-import uparrow from '../asset/uparrow.svg';
-import Link from "next/link";
-
+import navicon from '../asset/navicon.png'
+import uparrow from "../asset/uparrow.svg";
 
 const Nav = () => {
   return (
     <>
-      <div className="nav-group">
-        <Image src={logowhite} alt="Astrava" className="logowhite" />
+    <div className="nav-contain">
+    <div className="nav-wrap">
+        <Image src={navicon} alt="Arbistream-logo" className="logo"/>
         <div className="navbar-links">
-          <div className="nav-home">Home</div>
-          <div className="product-flex">
-            <div className="nav-home" id="product">Product</div>
-            <Image src={uparrow} alt="icon" className="uparrow" />
+            <div className="nav-home">Home</div>
+            <div className="product-flex">
+              <div className="nav-home" id="product">
+                Product
+              </div>
+              <Image src={uparrow} alt="icon" className="uparrow" />
+            </div>
+            <div className="product-flex">
+              <div className="nav-home" id="product">
+                Docs
+              </div>
+              <Image src={uparrow} alt="icon" className="uparrow" />
+            </div>
+            <div className="nav-home">Community</div>
           </div>
-          <div className="product-flex">
-            <div className="nav-home" id="product">Docs</div>
-            <Image src={uparrow} alt="icon" className="uparrow" />
-          </div>
-          <div className="nav-home">Community</div>
-        </div>
-        <Link href="/dex">
-          <button className="launch-btn">Launch App</button>
-        </Link>
-      </div>
+        <button className="connect-btn">Connect Wallet</button>
+    </div>
+    </div>
     </>
   )
 }

81f426b212c96fe20a5d8694c8afe1a91763ae5d
chayilDeborah <aniyikayeolamide3@gmail.com>
2023-06-21 03:41:26 +0100

    feat: create stream page and its contents

---
+++ app/stream/page.js
@@ -0,0 +1,62 @@
+"use client";
+import React, { useState } from "react";
+import { IconButton } from "@mui/material";
+import { Favorite, FavoriteBorder } from "@mui/icons-material";
+import Nav from "../component/Nav";
+import Image from "next/image";
+import profile from "../asset/profile.svg";
+import profilevideo from "../asset/profilevideo.svg";
+import love from "../asset/love.svg";
+
+// import comment from
+const Stream = () => {
+  const [liked, setLiked] = useState(false);
+
+  const handleLikeClick = () => {
+    setLiked(!liked);
+  };
+
+  return (
+    <>
+      <Nav />
+      <div className="stream-wrap">
+        <div>
+          <div className="profile-flex-grp">
+            <Image src={profile} alt="profile-img" className="profile-img" />
+            <div className="profile-names-flex">
+              <div className="profile-name">
+                Nakiya<span className="profile-span">@Nakiya</span>
+              </div>
+              <div className="profile-fam">
+                Family is all that matters. What did you do for thanksgiving.
+                Drop a comment.
+              </div>
+              <div className="profile-hastags">
+                #family #bonding #thanksgiving
+              </div>
+            </div>
+            <button className="profile-btn">Follow</button>
+          </div>
+          <div className="profile-video-flex">
+            <Image src={profilevideo} alt="video" className="prof-video" />
+            <div>
+              <div className="love-grp">
+                <IconButton onClick={handleLikeClick}>
+                  {liked ? (
+                    <Favorite sx={{ color: "red" }} />
+                  ) : (
+                    <FavoriteBorder />
+                  )}
+                </IconButton>
+                <div className="reaction-no">3.2k</div>
+              </div>
+              <div></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Stream;

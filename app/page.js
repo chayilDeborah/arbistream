7e448acc7981ecba15fbb7be0dae105b9b45002a
akins-dev <bobemmanuel503@gmail.com>
2023-06-22 05:12:04 +0100

    Done with landing page

--- app/page.js
+++ app/page.js
@@ -17,6 +17,7 @@ export default function Home() {
     <>
       <div className="wrapper" id="section1">
         <Nav />
+
         <div>
           <div
             className="page-flex"
@@ -65,7 +66,399 @@ export default function Home() {
             </div>
           </div>
         </div>
+
+        {/* section 2  */}
+        <div className="mt-[100px]">
+          <div className="text-white content-group mx-auto mb-[50px]"> Watch and Engage with Your Favourite Content</div>
+          <p className="text-white util mb-[50px]">Earn Rewards For Engagements in Astrava Tokens</p>
+          
+          <div className="flex overflow-scroll ml-5 mt-[50px] scrollbar-hide no-scrollbar">
+            <div className="relative h-96 min-w-[300px]">
+              <Image
+                  src="http://127.0.0.1:3000/img1.svg" 
+                  fill={true} 
+                  alt="Image"
+                  style={{objectFit: "cover"}}
+                  className="rounded-2xl"
+              />
+            </div>
+            <div className="relative h-96 min-w-[300px]">
+              <Image
+                  src="http://127.0.0.1:3000/img1.svg" 
+                  fill={true} 
+                  alt="Image"
+                  style={{objectFit: "cover"}}
+                  className="rounded-2xl"
+              />
+            </div>
+            <div className="relative h-96 min-w-[300px]">
+              <Image
+                  src="http://127.0.0.1:3000/img1.svg" 
+                  fill={true} 
+                  alt="Image"
+                  style={{objectFit: "cover"}}
+                  className="rounded-2xl"
+              />
+            </div>
+            <div className="relative h-96 min-w-[300px]">
+              <Image
+                  src="http://127.0.0.1:3000/img1.svg" 
+                  fill={true} 
+                  alt="Image"
+                  style={{objectFit: "cover"}}
+                  className="rounded-2xl"
+              />
+            </div>
+            <div className="relative h-96 min-w-[300px]">
+              <Image
+                  src="http://127.0.0.1:3000/img1.svg" 
+                  fill={true} 
+                  alt="Image"
+                  style={{objectFit: "cover"}}
+                  className="rounded-2xl"
+              />
+            </div>
+            <div className="relative h-96 min-w-[300px]">
+              <Image
+                  src="http://127.0.0.1:3000/img1.svg" 
+                  fill={true} 
+                  alt="Image"
+                  style={{objectFit: "cover"}}
+                  className="rounded-2xl"
+              />
+            </div>
+          </div>  
+
+          <div className="flex items-center mx-auto my-5 align-center">
+            <button className="bg-[#274A9D] mx-auto text-white font-semibold rounded-[5px] py-3 px-7">
+              Explore Top Creators
+            </button>
+          </div>
+        </div>
+        {/* end */}
+
+        {/* section 3 */}
+        <div className="flex-col text-white items-center mx-auto mt-[200px] py-[25px] bg-[#4F1763] ">
+          <div className="text-white content-group mx-auto mb-[16px]">Multiple Monitization Options</div>
+          <p className="text-white util mb-[50px]">Monitize Your content In Multiple Ways as a Creator</p>
+        </div>
+
+        <div className="flex mt-[75px] bg-background-black text-white justify-evenly mx-auto">
+          <div className="flex-col bg-[#8F2C91] h-[168.98px] w-[207px] rounded-[24px] mb-[20px] justify-center">
+            <div className="relative h-[60px] w-[60px] mt-[50px] mx-auto items-center">
+              <Image
+                  src="http://127.0.0.1:3000/direct.svg" 
+                  fill={true} 
+                  alt="Image"
+                  style={{objectFit: "fit"}}
+                  className=""
+              />
+            </div>
+            <div className="flex text-white font-semibold items-center mt-3 justify-center text-lg mb-[50px]">Direct Contributions</div>
+          </div>
+          <div className="flex-col bg-[#912C69] h-[168.98px] w-[207px] rounded-[24px] mb-[20px] justify-center">
+            <div className="relative h-[60px] w-[60px] mt-[50px] mx-auto items-center">
+              <Image
+                  src="http://127.0.0.1:3000/content.svg" 
+                  fill={true} 
+                  alt="Image"
+                  style={{objectFit: "fit"}}
+                  className=""
+              />
+            </div>
+            <div className="flex text-white font-semibold items-center mt-3 justify-center text-lg mb-[50px]">Sponsored Content</div>
+          </div>
+          <div className="flex-col bg-[#582C91] h-[168.98px] w-[207px] rounded-[24px] mb-[20px] justify-center">
+            <div className="relative h-[60px] w-[60px] mt-[50px] mx-auto items-center">
+              <Image
+                  src="http://127.0.0.1:3000/sharing.svg" 
+                  fill={true} 
+                  alt="Image"
+                  style={{objectFit: "fit"}}
+                  className=""
+              />
+            </div>
+            <div className="flex text-white font-semibold items-center mt-3 justify-center text-lg mb-[50px]">Revenue Sharing</div>
+          </div>
+          <div className="flex-col bg-[#26889E] h-[168.98px] w-[207px] rounded-[24px] mb-[20px] justify-center">
+            <div className="relative h-[60px] w-[60px] mt-[50px] mx-auto items-center">
+              <Image
+                  src="http://127.0.0.1:3000/adverts.svg" 
+                  fill={true} 
+                  alt="Image"
+                  style={{objectFit: "fit"}}
+                  className=""
+              />
+            </div>
+            <div className="flex text-white font-semibold items-center mt-3 justify-center text-lg mb-[50px]">Adverts placement</div>
+          </div>
+          <div className="flex-col bg-[#1C71A1] h-[168.98px] w-[207px] rounded-[24px] mb-[20px] justify-center">
+            <div className="relative h-[60px] w-[60px] mt-[50px] mx-auto items-center">
+              <Image
+                  src="http://127.0.0.1:3000/streaming.svg" 
+                  fill={true} 
+                  alt="Image"
+                  style={{objectFit: "fit"}}
+                  className=""
+              />
+            </div>
+            <div className="flex text-white font-semibold items-center justify-center text-lg">Real Yield from</div>
+            <div className="flex text-white font-semibold items-center justify-center text-lg mb-[50px]">Streaming</div>
+          </div>
+        </div>
+        {/* end */}
+
+        {/* section 4  */}
+        <div className="mt-[100px]">
+          <div className="text-white content-group mx-auto mb-[50px]"> Secure and Transparent Way To Trade Digital Assets</div>
+          <p className="text-white util mb-[50px]">Trade Easily, No Hidden Charges, Community Driven</p>
+        </div>
+
+        <div className="flex mt-[75px] bg-background-black text-white justify-evenly mx-auto">
+          <div className="flex-col bg-[#26569E] h-[168.98px] w-[207px] rounded-[24px] mb-[20px] items-center justify-center">
+            <div className="relative h-[60px] w-[60px] mt-[50px] mx-auto items-center">
+              <Image
+                  src="http://127.0.0.1:3000/bots.svg" 
+                  fill={true} 
+                  alt="Image"
+                  style={{objectFit: "fit"}}
+                  className=""
+              />
+            </div>
+            <div className="flex text-white font-semibold items-center justify-center mt-3 text-lg mb-[50px]">Alpha Caller Bots</div>
+          </div>
+          <div className="flex-col bg-[#26569E] h-[168.98px] w-[207px] items-center rounded-[24px] mb-[20px] justify-items-center">
+            <div className="relative h-[60px] w-[60px] mt-[50px] mx-auto items-center">
+              <Image
+                  src="http://127.0.0.1:3000/mask.svg" 
+                  fill={true} 
+                  alt="Image"
+                  style={{objectFit: "fit"}}
+                  className=""
+              />
+            </div>
+            <div className="flex text-white font-semibold mx-auto items-center justify-center text-lg">Streamlined</div>
+            <div className="flex text-white font-semibold mx-auto items-center justify-center text-lg mb-[50px]">Blockchain Updates</div>
+          </div>
+          <div className="flex-col bg-[#26569E] h-[168.98px] w-[207px] rounded-[24px] mb-[20px] justify-center">
+            <div className="relative h-[60px] w-[60px] mt-[50px] mx-auto items-center">
+              <Image
+                  src="http://127.0.0.1:3000/wallet.svg" 
+                  fill={true} 
+                  alt="Image"
+                  style={{objectFit: "fit"}}
+                  className=""
+              />
+            </div>
+            <div className="flex text-white font-semibold items-center mt-3 justify-center text-lg mb-[50px]">Wallet Monitoring</div>
+          </div>
+        </div>
+        {/* end */}
+
+        {/* section 5 */}
+        <div className="flex-col text-white items-center mx-auto mt-[200px] py-[25px] bg-[#4F1763] ">
+          <div className="text-white content-group mx-auto mb-[16px]">Platform Features</div>
+          <p className="text-white util mb-[50px]">Utilities and Well Though Out Features, Features Just for You</p>
+        </div>
+        <div className="relative h-[534px] w-[461px] mt-[50px] mx-auto items-center">
+          <Image
+              src="http://127.0.0.1:3000/img2.svg" 
+              fill={true} 
+              alt="Image"
+              style={{objectFit: "fit"}}
+              className=""
+          />
+        </div>
+        {/* end */}
+
+        {/* section 6 */}
+        <div className="relative h-[55px] w-[41px] mt-[200px] mx-auto items-center">
+          <Image
+              src="http://127.0.0.1:3000/mic.png" 
+              fill={true} 
+              alt="Image"
+              style={{objectFit: "fit"}}
+              className=""
+          />
+        </div>
+        <div className="my-[30px]">
+          <div className="text-white content-group mx-auto mb-[20px]">Our Partners</div>
+          <p className="text-white util mb-[50px]">We are building the Ecosystem with the help of these Great Teams</p>
+        </div>
+        <div className="grid grid-cols-3 items-center justify-items-center space-y-5 h-[243px] bg-gray-400">
+          <div className="relative w-[129px] h-[38px]">
+            <Image
+                src="http://127.0.0.1:3000/unipass1.svg" 
+                fill={true} 
+                alt="Image"
+                style={{objectFit: "fit"}}
+                className=""
+            />
+          </div>  
+          <div className="relative w-[129px] h-[38px] ">
+            <Image
+                src="http://127.0.0.1:3000/pinksale2.svg" 
+                fill={true} 
+                alt="Image"
+                style={{objectFit: "fit"}}
+                className=""
+            />
+          </div>  
+          <div className="relative w-[129px] h-[38px]">
+            <Image
+                src="http://127.0.0.1:3000/dexview.svg" 
+                fill={true} 
+                alt="Image"
+                style={{objectFit: "fit"}}
+                className=""
+            />
+          </div>  
+          <div className="relative w-[129px] h-[38px]">
+            <Image
+                src="http://127.0.0.1:3000/arbitrum2.svg" 
+                fill={true} 
+                alt="Image"
+                style={{objectFit: "fit"}}
+                className=""
+            />
+          </div>  
+          <div className="relative w-[129px] h-[38px] ">
+            <Image
+                src="http://127.0.0.1:3000/arbitrum2.svg" 
+                fill={true} 
+                alt="Image"
+                style={{objectFit: "fit"}}
+                className=""
+            />
+          </div>  
+          <div className="relative w-[129px] h-[38px] ">
+            <Image
+                src="http://127.0.0.1:3000/unipass2.svg" 
+                fill={true} 
+                alt="Image"
+                style={{objectFit: "fit"}}
+                className=""
+            />
+          </div>         
+        </div>
+        {/* end */}
+
+        {/* section 7 */}
+        <div className="flex-col text-white items-center mx-auto mt-[200px] py-[25px] bg-[#4F1763] ">
+          <div className="text-white content-group mx-auto mb-[16px]">Our RoadMap</div>
+          <p className="text-white util mb-[50px]">Anticipate Our Plans & RoadMap</p>
+          
+          <div className="space-y-5 mt-10 ">
+            <div className="flex rounded-lg h-[74px] max-w-[350px] mx-auto items-center justify-center outline outline-4 outline-white">
+              
+              <div className="relative ml-14 mr-2 h-[38px] w-[40px] justify-evenly items-center">
+                <Image
+                    src="http://127.0.0.1:3000/vector.png" 
+                    fill={true} 
+                    alt="Image"
+                    style={{objectFit: "fit"}}
+                    className=""
+                />
+            </div>
+            <p className="text-lg right-3 font-semibold">Platform Development Commencement</p>
+            </div>
+            
+            <div className="flex rounded-lg h-[74px] max-w-[350px] mx-auto items-center justify-center outline outline-4 outline-white">
+              <div className="relative h-[38px] w-[40px] mr-2 justify-evenly items-center">
+                <Image
+                    src="http://127.0.0.1:3000/vector2.png" 
+                    fill={true} 
+                    alt="Image"
+                    style={{objectFit: "fit"}}
+                    className=""
+                />
+              </div>
+              <p className="text-lg font-semibold">Community Building</p>
+            </div>
+
+            <div className="flex rounded-lg h-[74px] max-w-[350px] mx-auto items-center justify-center outline outline-4 outline-white">
+              <div className="relative h-[38px] w-[40px] mr-2 justify-evenly items-center">
+                <Image
+                    src="http://127.0.0.1:3000/awareness.png" 
+                    fill={true} 
+                    alt="Image"
+                    style={{objectFit: "fit"}}
+                    className=""
+                />
+              </div>
+              <p className="text-lg font-semibold">Awareness Creation</p>
+            </div>
+
+            <div className="flex rounded-lg h-[74px] max-w-[350px] mx-auto items-center justify-center outline outline-4 outline-white">
+              <div className="relative h-[38px] w-[40px] mr-2 justify-evenly items-center">
+                <Image
+                    src="http://127.0.0.1:3000/vested.png" 
+                    fill={true} 
+                    alt="Image"
+                    style={{objectFit: "fit"}}
+                    className=""
+                />
+              </div>
+              <p className="text-lg font-semibold">Vested private Sale</p>
+            </div>
+
+            <div className="flex rounded-lg h-[74px] max-w-[350px] mx-auto items-center justify-center outline outline-4 outline-white">
+              <div className="relative h-[38px] w-[40px] mr-2 justify-evenly items-center">
+                <Image
+                    src="http://127.0.0.1:3000/presale.png" 
+                    fill={true} 
+                    alt="Image"
+                    style={{objectFit: "fit"}}
+                    className=""
+                />
+              </div>
+              <p className="text-lg font-semibold">Public Presale</p>
+            </div>
+
+            <div className="flex rounded-lg h-[74px] max-w-[350px] mx-auto items-center justify-center outline outline-4 outline-white">
+              <div className="relative h-[38px] w-[40px] mr-2 justify-evenly items-center">
+                <Image
+                    src="http://127.0.0.1:3000/launch.png" 
+                    fill={true} 
+                    alt="Image"
+                    style={{objectFit: "fit"}}
+                    className=""
+                />
+              </div>
+              <p className="text-lg font-semibold">Platform Launch</p>
+            </div>
+
+            <div className="flex rounded-lg h-[74px] max-w-[350px] mx-auto items-center justify-center outline outline-4 outline-white">
+              <div className="relative h-[38px] w-[40px] mr-2 justify-evenly items-center">
+                <Image
+                    src="http://127.0.0.1:3000/creators.png" 
+                    fill={true} 
+                    alt="Image"
+                    style={{objectFit: "fit"}}
+                    className=""
+                />
+              </div>
+              <p className="text-lg font-semibold">Creators Onboarding</p>
+            </div>
+
+            <div className="flex rounded-lg h-[74px] max-w-[350px] mx-auto items-center justify-center outline outline-4 outline-white">
+              <div className="relative h-[38px] w-[40px] mr-2 justify-evenly items-center">
+                <Image
+                    src="http://127.0.0.1:3000/airdrop.png" 
+                    fill={true} 
+                    alt="Image"
+                    style={{objectFit: "fit"}}
+                    className=""
+                />
+              </div>
+              <p className="text-lg font-semibold">Token Airdrop</p>
+            </div>
+          </div>
+          
+        </div>
+        {/* end */}
       </div>
+
       <Footer id="section3" />
     </>
   );
